f2e1c34610333dc0268df58da2974e9b9d9c4e2b
Streed <dieppois.streed@gmail.com>
2020-09-15 19:48:13 +0200

    Ajout localStorage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from "react"
-import Todos from "./components/Todos"
+import React, { useState, useEffect } from "react";
+import Todos from "./components/Todos";
+import ModeSwitch from "./components/ModeSwitch";
+import AppContainer from "./components/AppContainer";
+import ModeContextProvider from "./context/ModeContext";
 
 function App() {
+  const [mode, setMode] = useState(
+    () => localStorage.getItem("mode") || "light"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("mode", mode);
+  }, [mode]);
+
   return (
-    <div className="container my-4">
-      <h1 className="text-center">ToDos App</h1>
-      <Todos />
-      <p className="mt-5">
-        Icons made by{" "}
-        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-          Freepik
-        </a>{" "}
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          {" "}
-          www.flaticon.com
-        </a>
-      </p>
-    </div>
-  )
+    <ModeContextProvider>
+      <AppContainer mode={mode}>
+        <div className="container my-4">
+          <h1 className="text-center">ToDos App</h1>
+          <ModeSwitch setMode={setMode} />
+          <Todos />
+        </div>
+      </AppContainer>
+    </ModeContextProvider>
+  );
 }
 
-export default App
+export default App;
